96f9c82b7ae68b526290a423076e75045f29fd72
mik284 <mikeykamau222@gmail.com>
2024-02-27 18:34:49 +0300

    add resolvers functions and dummy db

--- Lesson1/index.js
+++ Lesson1/index.js
@@ -1,23 +1,35 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-
-
+// import db
+import db from "./_db";
 
 import { typeDefs } from "./shema";
 
-
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 // server setup
 const server = new ApolloServer({
-// typedefs -------definations of types of data to expose #schema
-typeDefs,
-// resolvers ---handle incoming req and return to the client
-})
+  // typedefs -------definations of types of data to expose #schema
+  typeDefs,
+  // resolvers ---handle incoming req and return to the client
+  resolvers,
+});
 
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 40002 },
+});
 
-const {url} = await startStandaloneServer(server,{
-    listen: {port: 40002}
-})
-
-console.log('server ready at port ', 40002);
+console.log("server ready at port ", 40002);
